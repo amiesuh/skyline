00579a72f09cff0e8f139414bc006deff613f835
Rosie <rosauraher850@gmail.com>
2018-08-01 11:14:16 -0700

    profile

--- js/profile.js
+++ js/profile.js
@@ -1,1 +1,16 @@
+document.addEventListener('DOMContentLoaded', init);
+function init() {
+  var input = document.querySelector('input');
+  var reader = new FileReader();
 
+  // When the FileReader "load" event fires, update the image.
+  reader.onload = function (e) {
+      document.getElementById("image").src = e.target.result;
+  };
+
+  // Listen for when the input changes.
+  input.addEventListener('change', onInputChange)
+  function onInputChange(e) {
+    reader.readAsDataURL(this.files[0]);
+  }
+}
