1ae394e0256168d10e9a52066d75c588c3c42b8c
Rosie <rosauraher850@gmail.com>
2018-08-02 15:27:21 -0700

    bulletin

--- js/news.js
+++ js/news.js
@@ -16,3 +16,23 @@ window.onclick = function(event) {
     }
   }
 }
+
+
+function miFunction() {
+    document.getElementById("chats").classList.toggle("shows");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.chatrooms')) {
+
+    var dropdowns = document.getElementsByClassName("chats");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('shows')) {
+        openDropdown.classList.remove('shows');
+      }
+    }
+  }
+}
