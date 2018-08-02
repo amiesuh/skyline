39818c9e3d3b2e16028df55213092a02404b37a4
Rosie <rosauraher850@gmail.com>
2018-08-02 12:44:57 -0700

    split page

--- js/welln.js
+++ js/welln.js
@@ -36,23 +36,3 @@
       function stopVideo() {
         player.stopVideo();
       }
-
-
-
-
-
-
-      // var myIndex = 0;
-      // carousel();
-      //
-      // function carousel() {
-      //     var i;
-      //     var x = document.getElementsByClassName("theshow");
-      //     for (i = 0; i < x.length; i++) {
-      //        x[i].style.display = "none";
-      //     }
-      //     myIndex++;
-      //     if (myIndex > x.length) {myIndex = 1}
-      //     x[myIndex-1].style.display = "block";
-      //     setTimeout(carousel, 3000); // Change image every 2 seconds
-      // }
